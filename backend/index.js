5b4d2ec4e2b93a9a46dd8d1ae3d526e316aea4b2
sahil0233 <73436999+sahil0233@users.noreply.github.com>
2024-02-08 22:11:03 +0530

    cor configurations

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
 const app = express();
-app.use(cors());
+const options = [
+    cors({
+      origin: '*',
+      methods: '*',
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    })
+  ];
+  
+  app.use(options);
 app.use(express.json());
 app.use("/",mainRouter);
 
